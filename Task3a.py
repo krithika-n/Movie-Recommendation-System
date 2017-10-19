f3526fca76e802e89ae9934e6f74453393e1dc4e
skp1995 <shreeekrish@gmail.com>
2017-10-19 16:04:54 -0700

    Add files via upload

--- Task3a.py
+++ Task3a.py
@@ -1,10 +1,11 @@
 import sqlite3
+import csv
 import sys
 import math
 import datetime 
 import time
 import operator
-import numpy
+import numpy 
 
 from numpy import matrix
 conn = sqlite3.connect('projphase2.db')
@@ -15,6 +16,9 @@ c3 = conn.cursor()
 c4 = conn.cursor()
 c5 = conn.cursor()
 c6 = conn.cursor()
+c7 = conn.cursor()
+c8 = conn.cursor()
+
 
 Taglist = []
 Actorandtag = {}
@@ -23,9 +27,14 @@ c.execute("SELECT count(distinct ActorID) from Movieactor")
 TotalNoOfActors = c.fetchone()[0]
 #print(TotalNoOfActors)
 
-#tag vector for each actor
+k = 0
+actorIndex = {}
+
 for r in c.execute("SELECT distinct ActorID from Movieactor"):
     actid = r[0]
+    r11 = r[0]
+    actorIndex[r11] = k
+    k = k + 1
     Actorandtag[actid] = {}
     
     for r9 in c1.execute("SELECT distinct MovieID from Movieactor where ActorID = ?", (actid,)):
@@ -39,6 +48,7 @@ for r in c.execute("SELECT distinct ActorID from Movieactor"):
         for r1 in c2.execute("SELECT distinct TagID from Tags where MovieID = ?", (movid,) ):
             tagid = r1[0]
             Taglist.append(tagid)
+            #Actorandtag.append(tagid)
             #gets the count of eacg tag in this movie
             c5.execute("SELECT distinct count(TagID) from Tags where TagID = ? and MovieID = ?",(tagid,movid) )
             ThisMovieThisTagCount = c5.fetchone()[0]
@@ -73,7 +83,6 @@ for r in c.execute("SELECT distinct ActorID from Movieactor"):
             
             Actorandtag[actid][tagid] = TFIDF
             
-    
 def myprint(d):
     for key, value in d.items() :
         print ("Actor ID: " + key)
@@ -82,18 +91,58 @@ def myprint(d):
             print("\tScore:" + str(value1))
 
        
-#myprint(Actorandtag)      
+
+#myprint(Actorandtag)  
+
+
 from pandas import *
 A = DataFrame(Actorandtag).T.fillna(0)
 
 Atrans = numpy.transpose(A)
 
+
 Simmatrix = numpy.matmul(A, Atrans)
 
+
+normSimmatrix = Simmatrix / Simmatrix.max(axis=0)
+#print(normSimmatrix)
+
+pers = {}
 import networkx as nx
 
-#Conversion of matrix to graph
-G = nx.from_numpy_matrix(Simmatrix)
+G = nx.DiGraph(normSimmatrix.T)
+gWeighted = nx.DiGraph()
+
+for i, j  in G.edges:
+    gWeighted.add_edge(i, j, weight = normSimmatrix[i,j])
+
+seeds = []
+for i in sys.argv:
+    seeds.append(i) 
+
+seeds.remove(sys.argv[0])
+
+s1 = numpy.zeros((len(Simmatrix), 1))
+p = numpy.zeros((len(Simmatrix), 1))
+for i in seeds:
+    
+    j = actorIndex[i]
+    pers[j] = 1 / len(seeds)
+    s1[j][0] = 1 / len(seeds)
+    p[j][0] = 1
+
+    
+beta = 0.85
+term1 =  beta * numpy.matmul(Simmatrix, p)
+
+
+term2 = (1 - beta) * s1
+
+finalvector = term1 + term2
+
+print(finalvector)
 
 
 
+conn.commit()
+conn.close()
