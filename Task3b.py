80fdc3299a3cdcbb917f559067869c91005d58a4
rdipika94 <rdip94@gmail.com>
2017-10-22 18:11:54 -0700

    Final 3b.py
    removed graph,added output formatting

--- Task3b.py
+++ Task3b.py
@@ -1,8 +1,8 @@
 import mysql.connector
 import sys
 import numpy as np
-import networkx as nx
 
+actor_name={}
 no_actors_set=[]
 one_row_in_array=[]
 actorA_list=[]
@@ -23,6 +23,13 @@ try:
     # get the db cursor object to interact with db
     k=0
     cur = conn.cursor()
+
+    #map actorid to actor name
+    cur.execute(("SELECT id,name FROM mwdb.imdb_actor_info;"))
+    for row in cur.fetchall():
+        id=row[0]
+        name=row[1]
+        actor_name[id]=name;
     #get the number of actors
     cur.execute(("SELECT distinct (actorid) FROM mwdb.movie_actor;"))
     for row in cur.fetchall():
@@ -33,7 +40,7 @@ try:
         k=k+1
     no_actors=len(no_actors_set)
 
-    my_array=np.array([],float)              #initialize numpy array for the transition matrix
+    my_array=np.array([],float)              #initialize numpy array
     np.set_printoptions(threshold=np.nan)
 
     for actorA in no_actors_set:
@@ -56,12 +63,6 @@ try:
         sum_column = np.sum((my_array[:, i]))
         my_array[:, i] /= sum_column
 
-    #construct graph
-    g=nx.DiGraph(my_array.T)
-    weighted_g=nx.DiGraph()
-    for u, v in g.edges:
-        weighted_g.add_edge(u, v, weight=my_array[v, u])
-
     teleport = np.zeros((no_actors, 1))  # teleportation matrix
     prscore = np.zeros((no_actors, 1))  # matrix for pagerank scores
 
@@ -69,14 +70,15 @@ try:
     for i in sys.argv:
         names.append(i)
     names.remove(sys.argv[0])
+    print("List of seed actors-")
     for i in names:
         key=int(i)                                            #convert string cmd-line arg to int
-        personal_dict[actor_to_index[key]]= 1 / len(names)  #map each actorid to values in (0,308) in graph
+        personal_dict[actor_to_index[key]]= 1 / len(names)
         teleport[actor_to_index[key]][0] = 1 / len(names)
         prscore[actor_to_index[key]] = 1
-    print("personalization dict=", personal_dict)
+        print(actor_name[key])
 
-    #calculate pagerank with personalization
+   #calculate pagerank with personalization
 
     mean_error = 1
     alpha = 0.85
@@ -96,8 +98,7 @@ try:
         prevscore = prscore
         no_iter = no_iter + 1
 
-    print("Number of iterations -",no_iter)
-
+    print("\nNumber of iterations -",no_iter)
 
     #sort the pagerank scores and store the rank of the sorted values in sortlist
     sortlist = np.argsort(prscore, axis=0)
@@ -109,13 +110,14 @@ try:
         sortdict[k] = val[0]
         k = k + 1
 
-    #sort the dictionary and print the actorid for the top 10 ppr values
+    #sort the dictionary and print the actors with the top 10 ppr values
     k = 0
-    print("Top ten related actors to the seed set are:\n")
+    print("\nTop ten related actors to the seed set are:\n")
     for i in reversed(range(no_actors)):
         if (k < 10):
             actorpos = [key for key, value in sortdict.items() if value == i]
-            print(index_to_actor[actorpos[0]])
+            temp=index_to_actor[actorpos[0]]
+            print(actor_name[temp])
             k = k + 1
 
 except mysql.connector.Error as e:
